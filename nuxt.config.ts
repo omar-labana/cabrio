c8ca5e81655049c9e928e39f11e5650cddc49929
Omar Labana <omarlabana@outlook.com>
2024-05-10 14:40:28 +0300

    pipeline config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: "/cabrio/",
+  },
   ssr: false,
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/fonts", "nuxt-icon"],
 });
